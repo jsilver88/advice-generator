8a2632f750fff00ace9092c5871eb6575f7a4059
Jared Silver <jaredsilver@Jareds-MacBook-Pro.local>
2022-02-22 23:48:34 -0800

    added a try catch in the fetchAdvice function, deleted the fixed height on the container, added content in the h4, switched the h4 to a h1 for better semantics

--- js/app.js
+++ js/app.js
@@ -3,9 +3,13 @@ const quote = document.getElementById('quote')
 const adviceBtn = document.getElementById('advice-button')
 
 const fetchAdvice = async () => {
-  const response = await fetch(`https://api.adviceslip.com/advice`)
-  const data = await response.json()
-  renderAdvice(data)
+  try {
+    const response = await fetch(`https://api.adviceslip.com/advice`)
+    const data = await response.json()
+    renderAdvice(data)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const renderAdvice = (adviceData) => {
